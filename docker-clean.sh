f7bed0e312446e0249cb0153f7b042ae4c91d515
Tyrell Perera <tyrell.perera@gmail.com>
2016-05-21 08:59:39 +1000

    Update docker-clean.sh

--- docker-clean.sh
+++ docker-clean.sh
@@ -5,6 +5,3 @@ docker rm -v $(docker ps -a -q -f status=exited)
 
 # Delete dangling images
 docker rmi $(docker images -f "dangling=true" -q)
-
-# Remove unwanted volumes
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
